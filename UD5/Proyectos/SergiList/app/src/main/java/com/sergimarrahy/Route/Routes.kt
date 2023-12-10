025f743d81770a42638c94e1d18b930141bacdfd
Marrahy <sergimarenas@gmail.com>
2023-12-10 18:42:33 +0100

    10/12/23 - SergiList done

--- UD5/Proyectos/SergiList/app/src/main/java/com/sergimarrahy/Route/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.sergimarrahy.Route
-
-sealed class Routes(val route: String) {
-    object MainScreen: Routes("main_screen")
-    object WowCharacterInfo: Routes("wow_character_info_screen")
-}
