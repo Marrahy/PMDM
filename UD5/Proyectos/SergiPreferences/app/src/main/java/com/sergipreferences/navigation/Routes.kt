250ddc74e9d09141d3043792ffa2bbcb0a0ac76d
Marrahy <sergimarenas@gmail.com>
2023-12-29 16:09:37 +0100

    29/12/23 - SergiPreferences Done

--- UD5/Proyectos/SergiPreferences/app/src/main/java/com/sergipreferences/navigation/Routes.kt
+++
@@ -1,7 +0,0 @@
-package com.sergipreferences.navigation
-
-sealed class Routes(val route: String) {
-    object SplashScreen : Routes("splash_screen")
-    object LoginScreen : Routes("login_screen")
-    object MainScreen : Routes("main_screen")
-}
